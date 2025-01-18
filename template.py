35b78feb5f83a8e5f4639f8b8524232b07a4787e
comboonsong <piyabut_inb@hotmail.com>
2025-01-18 14:09:38 +0700

    rename folder because of typo

--- template.py
+++ template.py
@@ -4,13 +4,13 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "cnnClasifier"
+project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/__init__.gpy",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
